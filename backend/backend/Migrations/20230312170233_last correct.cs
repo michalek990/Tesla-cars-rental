5c79160d7fe25829ba0c4f23fec4160c36bd8c02
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-03-12 19:04:35 +0100

    workspace

---
+++ backend/backend/Migrations/20230312170233_last correct.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class lastcorrect : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VIN",
+                table: "Cars",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VIN",
+                table: "Cars",
+                type: "decimal(18,2)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17);
+        }
+    }
+}
