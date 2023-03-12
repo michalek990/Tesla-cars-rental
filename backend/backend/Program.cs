c889665147664d1b4e42f535d7b17e27d339dd47
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-03-12 19:52:04 +0100

    data base seeder

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,9 +1,11 @@
 using System.Reflection;
+using backend;
 using backend.Entity;
 using backend.Interfaces;
 using backend.Repositories;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +21,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<DataBaseSeeder>();
 
 var app = builder.Build();
 
@@ -28,7 +31,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<DataBaseSeeder>();
 
+
+seeder.Seed();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
