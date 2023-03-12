c08b7526fc9116904b5d6c1e699ab336c02834c7
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-03-12 18:06:11 +0100

    Implement dbContext in program.cs

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,8 +1,14 @@
+using backend.Entity;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation();
+builder.Services.AddDbContext<AppDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
