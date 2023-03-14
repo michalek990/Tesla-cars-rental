78652ba035011ea0ba73e5a210a9ba3feed02965
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-03-14 21:59:58 +0100

    CarRepository and RentalPointRepository

---
+++ backend/backend/Repositories/RentalPointRepository.cs
@@ -0,0 +1,24 @@
+﻿using backend.Entity;
+using backend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories;
+
+public class RentalPointRepository : BaseRepository<RentalPoint>, IRentalPointRepository
+{
+    public RentalPointRepository(AppDbContext context) : base(context) { }
+    
+    public async Task<RentalPoint?> GetRentalPointByName(string name)
+    {
+        return await Context.RentalPoints
+            .Where(r => r.RentalPointName == name)
+            .FirstOrDefaultAsync();
+    }
+    
+    public async Task<bool?> ExistByRentalPointName(string name)
+    {
+        return await Context.RentalPoints
+            .Where(r => r.RentalPointName == name)
+            .AnyAsync();
+    }
+}
