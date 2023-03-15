8e67129dc5956872fe8c96e7d20f15f3c96a34f0
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-03-15 01:05:09 +0100

    Migrations

---
+++ backend/backend/Migrations/20230314212803_rework records in rental table.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class reworkrecordsinrentaltable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PeselNumber",
+                table: "Rentals",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContactNumber",
+                table: "Rentals",
+                type: "int",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldMaxLength: 9);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PeselNumber",
+                table: "Rentals",
+                type: "decimal(18,2)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ContactNumber",
+                table: "Rentals",
+                type: "decimal(18,2)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 9);
+        }
+    }
+}
